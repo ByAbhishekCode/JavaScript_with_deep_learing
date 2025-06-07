8690cb4a67a71f599888f347799f12c662b25fbb
Abhishek <abhikumar1260@gmail.com>
2025-06-07 18:15:30 +0530

    end

--- if_else/script.js
+++ if_else/script.js
@@ -1,13 +1,15 @@
-const name = prompt("Enter the name")
-const age = prompt("Etner the age")
+const name = prompt("Enter the name") || "abhi";
+const age = prompt("Etner the age") || 19;
 
-console.log(`Name:-> ${name}`)
-console.log(`Name:-> ${age}`)
+console.log(`Name:-> ${name}`);
+console.log(`Name:-> ${age}`);
 
-if( age > 18 && age <= 22){
-    console.log(`${name} Go for Bike Ride`)
+if (age > 18 && age <= 22) {
+  console.log(`${name} Go for Bike Ride`);
+} else if (age > 23 && age <= 50) {
+  console.log(`${name} Go for the Car Ride`);
+} else if (age > 50 && age >= 100) {
+  console.log(`${name} Stay Home Safe Home`);
 }
 
-if(age > 23 && age >=50){
-    console.log(`${name} Go for the Car Ride`)
-}
+console.log("End ");
