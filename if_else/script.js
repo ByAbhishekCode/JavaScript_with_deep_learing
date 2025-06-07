d0601340e4a038d2123c8b3220711b073c713beb
Abhishek <127682284+ByAbhishekCode@users.noreply.github.com>
2025-06-07 12:11:05 +0000

    if else

---
+++ if_else/script.js
@@ -0,0 +1,13 @@
+const name = prompt("Enter the name")
+const age = prompt("Etner the age")
+
+console.log(`Name:-> ${name}`)
+console.log(`Name:-> ${age}`)
+
+if( age > 18 && age <= 22){
+    console.log(`${name} Go for Bike Ride`)
+}
+
+if(age > 23 && age >=50){
+    console.log(`${name} Go for the Car Ride`)
+}
