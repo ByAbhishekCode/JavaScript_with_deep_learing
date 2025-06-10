e22e8c8d62906488200e3ec6c193cc7f0fd3dd80
Abhishek <abhikumar1260@gmail.com>
2025-06-10 21:55:42 +0530

    hogya

---
+++ 04_object/objest_desruture.js
@@ -0,0 +1,8 @@
+const course = {
+    courseName : "js in hindi",
+    price: 999,
+    courseInstructor: "abhi"
+}
+
+const {courseInstructor: teacher , price : pr} = course
+console.log(teacher , pr)
