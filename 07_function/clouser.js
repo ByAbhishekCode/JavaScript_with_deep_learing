a9a72247e47f7aab804ec11d28f9729f71b8d0b6
Abhishek <abhikumar1260@gmail.com>
2025-06-13 11:23:25 +0530

    the end

--- 07_function/clouser.js
+++ 07_function/clouser.js
@@ -1,15 +1,31 @@
-function outer(){
-    let name = 'dada'
-    function inner(){
-        let name = 'papa'
-        console.log(name)
-        function inner2(){
-            console.log(name)
-        }
-        return inner2
-    }
-    return inner()
-}
+// function outer(){
+//     let name = 'dada'
+//     function inner(){
+//         let name = 'papa'
+//         console.log(name)
+//         function inner2(){
+//             console.log(name)
+//         }
+//         return inner2
+//     }
+//     return inner()
+// }
 
-let call_Outer = outer
-call_Outer()
+// let call_Outer = outer
+// call_Outer()
+
+function outer(){
+    let name = 'abhi'
+    let obj = {
+        name1 : "ram",
+        age :12
+    }
+    function inner(){
+        console.log(name)
+        console.log(obj)
+    }
+    return inner
+}
+const call = outer()
+call()
+
