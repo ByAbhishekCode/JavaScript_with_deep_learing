fdb5fb6bf645c6f5c2309a90f637603c317e21e2
Abhishek <abhikumar1260@gmail.com>
2025-06-14 11:23:03 +0530

    the end for forEach

--- 09_for_of_&&_for_each/forEach.js
+++ 09_for_of_&&_for_each/forEach.js
@@ -1,3 +1,13 @@
 const names = ["abhi", "ujjwal", "saurabh", "arpit", "abhinav", "satish"];
 
-names.forEach((name)=> {console.log(name)})
+// names.forEach((name) => console.log(name));
+
+const personDetails = {
+  firstName: "abhi",
+  lastName: "kumar",
+  age: 21,
+  city: "kanpur",
+};
+
+Object.entries(personDetails).forEach(([key,val]) => console.log(`this is key :-> ${key},
+    and this is its value :-> ${val}`))
