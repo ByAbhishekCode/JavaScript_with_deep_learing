5958282a0f785317b97bd0947d3759917f8c8f69
Abhishek <abhikumar1260@gmail.com>
2025-06-16 14:18:21 +0530

    this is for setlectors

---
+++ 12_DOM/script.js
@@ -0,0 +1,10 @@
+// function sayHi() {
+//   document.body.children[4].src =
+//     'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'
+// }
+
+let text0 = document.querySelector("#main").innerHTML
+let text1 = document.querySelector("#main").innerText;
+let text2 = document.querySelector("#main").textContent;
+
+let text4 = document.getElementById('#main')
