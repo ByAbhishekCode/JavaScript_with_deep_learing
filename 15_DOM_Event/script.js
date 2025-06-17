1d98b8992c78c87e62fdcea7e13c600fc2565fdf
Abhishek <abhikumar1260@gmail.com>
2025-06-17 16:03:08 +0530

    added

--- 15_DOM_Event/script.js
+++ 15_DOM_Event/script.js
@@ -1,12 +1,11 @@
-const h1 = document.querySelector('h1')
-const container = document.querySelector(".container")
+const container = document.querySelector('.container')
 const card = document.querySelector('.card')
+let count = 1
 
-// console.log(h1)
-
-for(let i = 2; i <= 1000; i++)
-    {
-        let add = card.cloneNode()
-        add.innerText = i
-        container.append("hi mai hu na")
-    }
+card.addEventListener("click", () =>{
+    let newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+    console.log(newCard)
+});
