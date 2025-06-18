0c2c853b83e352161628fd95dcb8bc3b6f6ec228
Abhishek <abhikumar1260@gmail.com>
2025-06-18 16:04:18 +0530

    for key event

---
+++ 17_DOM_keyboard/script.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1')
+const input = document.querySelector('input')
+
+document.addEventListener('keyup',(e)=>{
+    console.log(e.key)
+    console.log(e.keyCode)
+})
