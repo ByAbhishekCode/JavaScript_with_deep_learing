7a47dac3e6b17a60da9ff49d4ff3f916a560b005
Abhishek <abhikumar1260@gmail.com>
2025-06-20 08:25:25 +0530

    basics interview

---
+++ interviewQuesitons/03_basics.js
@@ -0,0 +1,12 @@
+function foo(){
+    console.log('hi')
+}
+
+const reult = foo()// in this line it will run foo beacuse of () this and not return any value then it will undefiend
+console.log(reult) // undefinet beacue of it will not return
+
+
+let arry1 = [4,3,5,2,6,8,1,2,]
+let graterarry = arry1.filter((num)=>{return num > 3})
+console.log(graterarry)
+
