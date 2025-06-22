aec01ed9d1fcb7bcac415ddee47dd52d129cd33c
Abhishek <abhikumar1260@gmail.com>
2025-06-22 13:59:23 +0530

    some changes

--- 20_callBack_hell/script.js
+++ 20_callBack_hell/script.js
@@ -27,39 +27,51 @@ function makeHttpRequest(method, url) {
   return promis;
 }
 
-makeHttpRequest("GET", "https://dummyjson.com/users")
-  .then((userData) => {
-    console.log(
-      "this is use id =",
-      userData.users[0].id,
-      " this is user name = ",
-      userData.users[0].firstName
-    );
-    return makeHttpRequest(
-      "GET",
-      `https://dummyjson.com/posts/user/${userData.users[0].id}`
-    );
-  })
-  .then((postData) => {
-    console.log(
-      "this is user post = ",
-      postData.posts[0].id,
-      " this is user post title =",
-      postData.posts[0].title
-    );
-    return makeHttpRequest(
-      "GET",
-      `https://dummyjson.com/comments/post/${postData.posts[0].id}`
-    );
-  })
-  .then((commetData) => {
-    console.log(
-      "this is comment id = ",
-      commetData.comments[0].id,
-      "this is comment body or title = ",
-      commetData.comments[0].body
-    );
-  });
+
+fetch("https://dummyjson.com/users").then((res)=> res.json())
+.then((data)=>{
+  console.log(data)
+  return fetch(`https://dummyjson.com/posts/user/${userData.users[0].id}`)
+})
+// .then((res)=>
+//   res.json()).then((data)=>{console.log(data)})
+
+
+
+
+// makeHttpRequest("GET", "https://dummyjson.com/users")
+//   .then((userData) => {
+//     console.log(
+//       "this is use id =",
+//       userData.users[0].id,
+//       " this is user name = ",
+//       userData.users[0].firstName
+//     );
+//     return makeHttpRequest(
+//       "GET",
+//       `https://dummyjson.com/posts/user/${userData.users[0].id}`
+//     );
+//   })
+//   .then((postData) => {
+//     console.log(
+//       "this is user post = ",
+//       postData.posts[0].id,
+//       " this is user post title =",
+//       postData.posts[0].title
+//     );
+//     return makeHttpRequest(
+//       "GET",
+//       `https://dummyjson.com/comments/post/${postData.posts[0].id}`
+//     );
+//   })
+//   .then((commetData) => {
+//     console.log(
+//       "this is comment id = ",
+//       commetData.comments[0].id,
+//       "this is comment body or title = ",
+//       commetData.comments[0].body
+//     );
+//   });
 
 // this is callback hell
 // makeHttpRequest("GET", "https://dummyjson.com/users", (userData) => {
@@ -80,3 +92,31 @@ makeHttpRequest("GET", "https://dummyjson.com/users")
 //     }
 //   );
 // });
+
+
+fetch("https://dummyjson.com/users")
+  .then((res) => res.json())
+  .then((userData) => {
+    const userId = userData.users[0].id;
+    const userName = userData.users[0].firstName;
+    console.log(`User ID = ${userId}, Name = ${userName}`);
+
+    return fetch(`https://dummyjson.com/posts/user/${userId}`);
+  })
+  .then((res) => res.json())
+  .then((postData) => {
+    const postId = postData.posts[0].id;
+    const postTitle = postData.posts[0].title;
+    console.log(`Post ID = ${postId}, Title = ${postTitle}`);
+
+    return fetch(`https://dummyjson.com/comments/post/${postId}`);
+  })
+  .then((res) => res.json())
+  .then((commentData) => {
+    const commentId = commentData.comments[0].id;
+    const commentBody = commentData.comments[0].body;
+    console.log(`Comment ID = ${commentId}, Body = ${commentBody}`);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
--- 22_fetch/script.js
+++ 22_fetch/script.js
@@ -1,1 +1,13 @@
-"https://dummyjson.com/users";
+fetch("https://dummyjson.com/users")
+.then((res)=>  res.json())
+.then((data)=> console.log(data))
+
+
+fetch("https://dummyjson.com/products/add",{
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'abhi',
+        hero: 'like superman'
+    })
+}).then((res => res.json()))
+.then(console.log)
