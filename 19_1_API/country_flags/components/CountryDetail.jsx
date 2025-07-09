375d98f068fe9f1ed9ef2054318a189fa3da75fe
Abhishek <abhikumar1260@gmail.com>
2025-07-09 17:21:53 +0530

    add back button and dynamic rounting

--- 19_1_API/country_flags/components/CountryDetail.jsx
+++ 19_1_API/country_flags/components/CountryDetail.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useParams } from "react";
 
 import './CountryDetail.css'
 
 export default function CountryDetail() {
-  const countryName = new URLSearchParams(location.search).get('name')
+  const params = useParams()
+  const countryName = params.country
 
   const [countryData, setCountryData] = useState(null)
+  const [notFound,setNotFound] = useState(false)
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
@@ -27,14 +29,18 @@ export default function CountryDetail() {
             .join(', '),
           
         })
-      })
+      }).catch(err => setNotFound(true))
   }, [])
+
+  if(notFound){
+    return <div>Coutnry not found</div>
+  }
   return countryData === null ? (
     'loading...'
   ) : (
     <main>
       <div className="country-details-container">
-        <span className="back-button">
+        <span className="back-button" onClick={()=>history.back()}>
           <i className="fa-solid fa-arrow-left"></i>&nbsp; Back
         </span>
         <div className="country-details">
